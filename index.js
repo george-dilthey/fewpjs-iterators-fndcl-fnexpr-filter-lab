9e510c7d779d5f486b79b36a64655b2a01faf3ca
George Dilthey <george.dilthey@gmail.com>
2021-08-22 16:03:52 -0400

    commit tree 3a58bbd0c7313184caf32d5291a4298bf4b150a3

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(arr, str){
+    return arr.filter(v => v.toLowerCase() === str.toLowerCase())
+}
+
+function fuzzyMatch(arr, str){
+    return arr.filter(d => d.substring(0, str.length) === str)
+}
+
+function matchName(arr, str){
+    return arr.filter(d => d.name === str)
+}
